f64374ad76d73ee5367e12d581620e63d57b1e82
prasetion <prasetion@gmail.com>
2024-06-26 20:18:18 +0700

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue"; // or any other framework plugin you're using
 
 export default defineConfig({
-  plugins: [vue()],
   assetsInclude: ["**/*.glb", "**/*.hdr"], // Add this line if not already present
 });
